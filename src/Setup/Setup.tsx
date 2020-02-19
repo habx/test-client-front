3a82118b2dd81a1ece6e03b3ad177b558bc29a16
Jean Dessane <jean@habx.fr>
2020-02-19 16:35:20 +0100

    improvements

--- src/Setup/Setup.tsx
+++ src/Setup/Setup.tsx
@@ -5,10 +5,9 @@ import { Button, Title } from '@habx/ui-core'
 
 import { projectQuery } from './Setup.query'
 import { SetupContainer } from './Setup.style'
-import { project } from './types/project'
 
-const Setup: React.FunctionComponent<SetupInterface> = () => {
-  const projectResponse = useQuery<project>(projectQuery)
+const Setup = () => {
+  const projectResponse = useQuery(projectQuery)
   return (
     <SetupContainer>
       <Title type="article">{projectResponse.data?.project.name}</Title>
@@ -17,7 +16,5 @@ const Setup: React.FunctionComponent<SetupInterface> = () => {
     </SetupContainer>
   )
 }
-
-interface SetupInterface {}
 
 export default Setup
